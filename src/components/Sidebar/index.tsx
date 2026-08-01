eca4d9eafa109f7e35f6836923d6e2916db29244
alirezashojaian1987 <alirezashojaian22@gmail.com>
2026-08-01 23:38:32 +0330

    Sidebar structure changed

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -5,14 +5,31 @@ import styles from "./Sidebar.module.scss";
 export default function Sidebar(){
     return(
         <aside className={styles.Sidebar}>
-            <nav>
-                {sidebarLinks.map(({title,path,icon:Icon})=>(
-                    <NavLink key={path} to={path}>
-                        <Icon/>
-                        <span>{title}</span>
-                    </NavLink>
-                ))}
+            <nav className={styles.Navigation}>
+                <ul className={styles.Nav_list}>
+                    {sidebarLinks.map(({title, path, icon:Icon})=>(
+                        <li key={path}>
+                            <NavLink
+                                to={path}
+                                className={({isActive})=>
+                                    isActive
+                                        ? `${styles.Nav_link} ${styles.Active}`
+                                        : styles.Nav_link
+                                }
+                                end={path==="/"}
+                            >
+                                <Icon className={styles.Icon}/>
+
+                                <span>{title}</span>
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
             </nav>
+
+            <footer className={styles.Footer}>
+                Mine Panel v1.0
+            </footer>
         </aside>
     );
 }
