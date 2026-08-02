1c4ab75b3e59cb6b0a636d36e7e0121c6c031b12
alirezashojaian1987 <alirezashojaian22@gmail.com>
2026-08-02 19:30:32 +0330

    Added menu hamburger for sidebar when on responsive view

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,23 @@
-
-
 import styles from "./Header.module.scss";
 import Minecraft_logo from "../../assets/images/mc_logo.png";
+import { IoMenuOutline } from "react-icons/io5";
 
-export default function Header(){
+interface HeaderProps{
+    onToggleSidebar?:()=>void;
+}
+
+export default function Header({onToggleSidebar}:HeaderProps){
     return(
         <header className={styles.Header}>
             <div className={styles.Logo_container}>
+                <button
+                    className={styles.MenuButton}
+                    onClick={onToggleSidebar}
+                    aria-label="Toggle navigation"
+                >
+                    <IoMenuOutline/>
+                </button>
+
                 <img
                     src={Minecraft_logo}
                     alt="Minecraft logo"
--- src/layouts/MainLayout/Main_layout.tsx
+++ src/layouts/MainLayout/Main_layout.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Outlet } from "react-router-dom";
 import styles from "./Main_layout.module.scss";
 
@@ -6,12 +8,27 @@ import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
 
 export default function Main_layout(){
+    const [sidebarOpen, setSidebarOpen]=useState(false);
+
+    const toggleSidebar = () => setSidebarOpen((prev)=>!prev);
+
     return(
         <div className={styles.layout}>
-            <Header/>
+            <Header onToggleSidebar={toggleSidebar}/>
 
             <div className={styles.Main_content}>
-                <Sidebar/>
+                <div
+                    className={`${styles.SidebarWrapper} ${sidebarOpen ? styles.Open : ""}`}
+                >
+                    <Sidebar/>
+                </div>
+
+                {sidebarOpen && (
+                    <div 
+                        className={styles.Overlay} 
+                        onClick={() => setSidebarOpen(false)}
+                    />
+                )}
 
                 <main className={styles.Content}>
                     <Outlet/>
