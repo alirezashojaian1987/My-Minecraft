c29d8ab0bbdf8239b57224765d47da51ffd828f2
alirezashojaian1987 <alirezashojaian22@gmail.com>
2026-08-08 17:37:29 +0330

    Tailwind added, imported in the config.ts and the main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from "./routes/Routes";
 
-import "./styles/Global.scss";
+import "./styles/Global.css";
 
 
 createRoot(document.getElementById('root')!).render(
